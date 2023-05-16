9fdb189886efe965f2bc4455c4a2094cb450965f
VoThiKimTrang06101997 <vothikimtrang0610@gmail.com>
2023-05-17 03:24:06 +0700

    user_homepage

---
+++ src/main/java/com/trang/MobileShop/configuration/TilesConfiguration.java
@@ -0,0 +1,25 @@
+package com.trang.MobileShop.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+public class TilesConfiguration extends WebMvcConfigurerAdapter {
+	@Bean
+	public UrlBasedViewResolver urlBasedViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setViewClass(TilesView.class);
+		return resolver;
+	}
+
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer configurer = new TilesConfigurer();
+		configurer.setDefinitions(new String[] { "/WEB-INF/tiles/tiles_user.xml" });
+		return configurer;
+	}
+}
